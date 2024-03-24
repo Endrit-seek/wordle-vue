8052d850d01c045b8e33562fec7d171a6ac72957
Endrit Krasniqi <esl.endritkrasniqi@gmail.com>
2024-03-24 17:54:04 +0100

    feat: make victory message a dynamic const

--- src/components/WordleBoard.vue
+++ src/components/WordleBoard.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
+import { VICTORY_MESSAGE } from "@/settings";
+
 defineProps<{
   wordOfTheDay: string;
 }>();
 </script>
 
 <template>
-  <input type="text">
-  <p>You Won!</p>
+  <input type="text" />
+  <p>{{ VICTORY_MESSAGE }}</p>
 </template>
-
