30a1310ee2c4578cf46265bd2c4c54d9413c2361
Endrit Krasniqi <esl.endritkrasniqi@gmail.com>
2024-03-25 03:23:43 +0100

    feat: extract GuessView component

--- src/components/GuessInput.vue
+++ src/components/GuessInput.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { WORD_SIZE } from "@/settings";
+import GuessView from "./GuessView.vue";
 import englishWords from "@/englishWordsWith5Letters.json";
 import { ref, computed } from "vue";
 
@@ -44,15 +45,7 @@ function onInput(event: Event) {
 </script>
 
 <template>
-  <ul class="word">
-    <li
-      v-for="(letter, index) in formattedGuessInProgress.padEnd(WORD_SIZE, ' ')"
-      :key="`${letter}-${index}`"
-      :data-letter="letter"
-      class="letter"
-      v-text="letter"
-    />
-  </ul>
+  <GuessView :guess="formattedGuessInProgress" />
 
   <input
     v-model="formattedGuessInProgress"
@@ -69,38 +62,5 @@ function onInput(event: Event) {
 input {
   position: absolute;
   opacity: 0;
-}
-
-.word {
-  list-style: none;
-  padding: 0;
-  display: flex;
-  gap: 0.25rem;
-}
-
-.letter {
-  background-color: white;
-  border: 1px solid hsl(0, 0%, 70%);
-  width: 5rem;
-  height: 5rem;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 2rem;
-  font-weight: bolder;
-}
-
-li:not([data-letter=" "]) {
-  animation: pop 100ms;
-}
-
-@keyframes pop {
-  0% {
-    transform: scale(1);
-  }
-
-  50% {
-    transform: scale(1.4);
-  }
 }
 </style>
