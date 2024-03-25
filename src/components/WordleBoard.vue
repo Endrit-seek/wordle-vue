b55faf1783edbd32ab85782bbf99124452ce71a0
Endrit Krasniqi <esl.endritkrasniqi@gmail.com>
2024-03-25 03:11:17 +0100

    feat: extract conditional

--- src/components/WordleBoard.vue
+++ src/components/WordleBoard.vue
@@ -1,10 +1,10 @@
 <script lang="ts" setup>
 import { DEFEAT_MESSAGE, VICTORY_MESSAGE, MAX_GUESSES_COUNT } from "@/settings";
 import englishWords from "@/englishWordsWith5Letters.json";
-import { ref } from "vue";
+import { ref, computed } from "vue";
 import GuessInput from "@/components/GuessInput.vue";
 
-defineProps({
+const props = defineProps({
   wordOfTheDay: {
     type: String,
     required: true,
@@ -13,6 +13,12 @@ defineProps({
 });
 
 const guessesSubmitted = ref<string[]>([]);
+
+const isGameOver = computed(
+  () =>
+    guessesSubmitted.value.length === MAX_GUESSES_COUNT ||
+    guessesSubmitted.value.includes(props.wordOfTheDay)
+);
 </script>
 
 <template>
@@ -20,10 +26,7 @@ const guessesSubmitted = ref<string[]>([]);
     <guess-input @guess-submitted="(guess) => guessesSubmitted.push(guess)" />
 
     <p
-      v-if="
-        guessesSubmitted.length === MAX_GUESSES_COUNT ||
-        guessesSubmitted.includes(wordOfTheDay)
-      "
+      v-if="isGameOver"
       class="end-of-game-message"
       v-text="
         guessesSubmitted.includes(wordOfTheDay)
